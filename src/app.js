e3efdfad26a688fa06775c3515fbcb58944671fa
sky <sky_bro@qq.com>
2019-04-10 22:57:14 +0800

    try AES and SHA256

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const CryptoJS = require('crypto-js');
 const AES = require('crypto-js/aes');
 const SHA256 = require('crypto-js/sha256');
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const hbs = require('hbs');
 
@@ -19,15 +20,46 @@ hbs.registerPartials(partialsPath);
 
 // setup static dir to serve
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
 let database = {
-    "user01": CryptoJS.SHA256('user01'+'pass01')
+    "sky": CryptoJS.SHA256('sky'+'pass01').toString()
 }
 
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Home'
     });
+});
+
+app.post('/data', (req, res) => {
+    console.log("body", req.body);
+    // let data_obj = JSON.parse(req.body);
+    let username = req.body.username;
+    let hash2 = req.body.hash2;
+    let rand1 = req.body.rand1;
+    if (username && hash2 && rand1){
+        let hash1 = database[username];
+        let hash2_new = CryptoJS.SHA256(hash1 + rand1).toString();
+        if (hash2_new === hash2){
+            res.json({
+                status: 'success',
+                rand2_encrypted: CryptoJS.AES.encrypt('123', hash1).toString()
+                // CryptoJS.lib.WordArray.random
+                // CryptoJS.AES.decrypt("U2FsdGVkX1+/3S3tj3nWXHFo7Chmm4uCDpGQFjAXBmo=", hash1).toString(CryptoJS.enc.Utf8)
+            })
+        } else {
+            res.json({
+                status: 'failed',
+                error: 'authentication failed'
+            })
+        }
+    } else {
+        res.json({
+            status: 'failed',
+            error: "please post username, hash2, rand1 to /data"
+        })
+    }
 });
 
 let port = process.env.port?process.env.port:3000;
